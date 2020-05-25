6e8576c104d80db5a9c949de75af4c1c16d2d443
Ben Keener <keener4christ@gmail.com>
2020-05-25 16:43:44 -0400

    Changed text

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello Cruel World!'))
+app.get('/', (req, res) => res.send('Hello, Travis!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
